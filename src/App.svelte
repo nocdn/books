83dd305c401e957fe1e64cb0a2461be6fe535dc6
nocdn <bartek.bak@protonmail.com>
2025-07-13 20:05:29 +0100

    feat: pulled out all logic from bookmarks component because its not needed

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import Navbar from "./Navbar.svelte";
-  import Bookmarks from "./Bookmarks.svelte";
+  import { Plus } from "lucide-svelte";
+  import Bookmark from "./Bookmark.svelte";
+  import Spinner from "./lib/Spinner.svelte";
   let backendAddress = $state("");
 
   $effect(() => {
@@ -105,6 +107,98 @@
     console.log(data);
     fetchBookmarks();
   }
+
+  let input = $state("");
+  let isLoading = $state(false);
+  // While request is pending, input text is gray
+  let isPending = $state(false);
+  // After success, trigger fade-out animation
+  let isFading = $state(false);
+  let inputElement: HTMLInputElement;
+
+  const parsedTags = $derived(
+    input.match(/#([\w-]+)/g)?.map((t) => t.substring(1)) ?? [],
+  );
+  const url = $derived(input.replace(/#([\w-]+)/g, "").trim());
+
+  function handleKeydown(event: KeyboardEvent) {
+    if (event.key === "Enter" && (event.metaKey || event.ctrlKey)) {
+      let submittedUrl = url;
+      if (!/^https?:\/\//i.test(submittedUrl)) {
+        submittedUrl = `https://${submittedUrl}`;
+      }
+      // Start spinner & mark as pending
+      isLoading = true;
+      isPending = true;
+
+      handleCreateBookmark({
+        url: submittedUrl,
+        tags: parsedTags,
+        createdAt: new Date().toLocaleString("en-GB", {
+          timeZone: "Europe/London",
+        }),
+      })
+        .then(() => {
+          isLoading = false;
+          isPending = false;
+
+          isFading = true;
+
+          setTimeout(() => {
+            input = "";
+            isFading = false;
+          }, 120);
+        })
+        .catch(() => {
+          isLoading = false;
+          isPending = false;
+          isFading = false;
+        });
+    }
+  }
+
+  function handleFileUpload() {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "text/plain, image/*, audio/*";
+    input.onchange = (e) => {
+      const file = (e.target as HTMLInputElement).files?.[0];
+      console.log(file);
+    };
+    input.click();
+  }
+
+  $effect(() => {
+    const handleGlobalKeydown = (event: KeyboardEvent) => {
+      if (event.key === "/" && inputElement) {
+        const target = event.target as HTMLElement;
+        if (target.tagName !== "INPUT") {
+          event.preventDefault();
+          inputElement.focus();
+        }
+      } else if (event.key === "Escape" && inputElement) {
+        inputElement.blur();
+      }
+    };
+
+    window.addEventListener("keydown", handleGlobalKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleGlobalKeydown);
+    };
+  });
+
+  let filteredBookmarks = $derived(
+    bookmarks.filter(
+      (bookmark: Bookmark) =>
+        bookmark.title.toLowerCase().includes(input.toLowerCase()) ||
+        bookmark.tags.some((tag) =>
+          tag.toLowerCase().includes(input.toLowerCase()),
+        ) ||
+        bookmark.url.toLowerCase().includes(input.toLowerCase()) ||
+        bookmark.createdAt.toLowerCase().includes(input.toLowerCase()),
+    ),
+  );
 </script>
 
 <main>
@@ -113,10 +207,56 @@
     onBackendChange={handleBackendChange}
     backendAddress={backendAddress} />
   <div class="mt-24 grid w-full place-items-center">
-    <Bookmarks
-      bookmarks={bookmarks}
-      onCreateBookmark={handleCreateBookmark}
-      onDeleteBookmark={deleteBookmark}
-      onEditBookmark={editBookmark} />
+    <div class="flex w-3xl flex-col gap-4">
+      <div
+        class="group flex items-center gap-2 rounded-lg border border-gray-200 p-3 focus-within:outline-2 focus-within:outline-offset-1 focus-within:outline-gray-700/45">
+        <div
+          role="button"
+          tabindex="0"
+          class="mr-0.5 grid h-5 w-5 cursor-pointer place-items-center opacity-50 transition-opacity duration-200 group-focus-within:opacity-100 group-hover:opacity-100"
+          onmousedown={handleFileUpload}>
+          {#if isLoading}
+            <Spinner size={22} />
+          {:else}
+            <Plus size={18} />
+          {/if}
+        </div>
+        <input
+          type="text"
+          placeholder="DREAM BIG, START SMALL"
+          class="font-jetbrains-mono w-full bg-transparent text-[15px] leading-none font-medium outline-none {isFading
+            ? 'motion-opacity-out-0 motion-duration-100'
+            : ''} {isPending ? 'text-gray-500' : ''}"
+          bind:value={input}
+          onkeydown={handleKeydown}
+          bind:this={inputElement} />
+      </div>
+      <div
+        class="flex h-0 items-center gap-2 transition-all duration-200 {parsedTags.length >
+        0
+          ? 'h-6'
+          : 'h-0'}">
+        {#each parsedTags as tag}
+          <p
+            class="font-jetbrains-mono motion-opacity-in-0 -motion-translate-y-in-[10%] motion-duration-300 rounded-sm bg-[#EAF8EF] px-1.5 py-0.5 text-[15px] font-medium text-[#00C460]">
+            {tag}
+          </p>
+        {/each}
+      </div>
+      <div
+        class="font-geist-mono flex items-center justify-between border-b border-gray-200 py-3 text-sm text-gray-400">
+        <p class="font-medium">TITLE</p>
+        <p>CREATED AT</p>
+      </div>
+      <div class="flex flex-col gap-3">
+        {#each filteredBookmarks as bookmark, index (bookmark.id)}
+          <Bookmark
+            bookmark={bookmark}
+            onDeleteBookmark={deleteBookmark}
+            onEditBookmark={editBookmark}
+            index={index} />
+        {/each}
+      </div>
+    </div>
   </div>
 </main>
